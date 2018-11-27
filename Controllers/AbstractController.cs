782fc40950d11fbb3beaef37fbbfe8986c6ec6c2
Radek12345 <gosciminskiradoslaw@gmail.com>
2018-11-28 00:41:32 +0100

    Create abstract rest controller

---
+++ Controllers/AbstractController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Persons.Core;
+using Persons.Core.Repositories;
+using System.Collections.Generic;
+
+namespace Persons.Controllers
+{
+    public abstract class AbstractController<TRepository, TEntity, TReadResource> : Controller where TEntity : class where TRepository : IRepository<TEntity>
+    {
+        protected readonly TRepository repository;
+        protected readonly IMapper mapper;
+        protected readonly IUnitOfWork unitOfWork;
+
+        public AbstractController(TRepository repository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IEnumerable<TReadResource> getAll()
+        {
+            return mapper.Map<IEnumerable<TEntity>, IEnumerable<TReadResource>>(repository.GetAll());
+        }
+    }
+}
--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -4,28 +4,13 @@ using Persons.Controllers.Resources;
 using Persons.Core;
 using Persons.Core.Domain;
 using Persons.Core.Repositories;
-using System.Collections.Generic;
 
 namespace Persons.Controllers
 {
     [Route("/api/cities")]
-    public class CitiesController
+    public class CitiesController : AbstractController<IRepository<City>, City, KeyValuePairResource>
     {
-        private readonly IRepository<City> repository;
-        private readonly IMapper mapper;
-        private readonly IUnitOfWork unitOfWork;
-
         public CitiesController(IRepository<City> repository, IMapper mapper, IUnitOfWork unitOfWork)
-        {
-            this.repository = repository;
-            this.mapper = mapper;
-            this.unitOfWork = unitOfWork;
-        }
-
-        [HttpGet]
-        public IEnumerable<KeyValuePairResource> GetCities()
-        {
-            return mapper.Map<IEnumerable<City>, IEnumerable<KeyValuePairResource>>(repository.GetAll());
-        }
+            : base(repository, mapper, unitOfWork) { }
     }
 }
